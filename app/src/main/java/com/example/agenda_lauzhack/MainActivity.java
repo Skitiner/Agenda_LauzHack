55913b629374ce5004d247b83f2d0d0470f61022
Skitiner <cod.skip@gmail.com>
2020-04-04 18:56:08 +0200

    ProfileActivity

--- app/src/main/java/com/example/agenda_lauzhack/MainActivity.java
+++ app/src/main/java/com/example/agenda_lauzhack/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.agenda_lauzhack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Profile userProfile = new Profile(); // TODO: 04.04.2020 vérifier si un profil existe deja 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
         // BAVE DE PANDA
     }
+
+    public void clickedProfileButtonXmlCallback(View view) {
+
+        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+        intent.putExtra(ProfileActivity.USER_PROFILE, userProfile);
+        startActivity(intent);
+
+    }
+
 }
