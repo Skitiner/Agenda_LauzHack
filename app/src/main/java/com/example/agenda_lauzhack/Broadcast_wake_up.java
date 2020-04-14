5a253b298ad477315da7161ce75a54f29fd474f0
Grégoire Tomassi <gregtom1997@gmail.com>
2020-04-14 20:01:20 +0200

    Notification: fully working

---
+++ app/src/main/java/com/example/agenda_lauzhack/Broadcast_wake_up.java
@@ -0,0 +1,41 @@
+package com.example.agenda_lauzhack;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class Broadcast_wake_up  extends BroadcastReceiver {
+
+    private String CHANNEL_ID = "";
+    private int notificationId = 0;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent start = new Intent(context, AgendaActivity.class);
+        start.putExtra("POPUP", true);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, start, 0);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.organisemois)
+                .setContentTitle("WAKE UP !")
+                .setContentText("Go in the application to check the planning of the day.")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Go in the application to check the planning of the day."))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .addAction(R.drawable.cheetah_background, "I am awake", pendingIntent)
+                .addAction(R.drawable.rabbit_background, "15 min more", pendingIntent)
+                .addAction(R.drawable.rabbit_background, "Cancel", pendingIntent);
+
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(notificationId, builder.build());
+    }
+}
